423f8a0989ca8456d08e9b808bdd6c53ecd65fd9
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-01 15:37:36 +0000

    Adding the base service provider

---
+++ src/LaravelLangServiceProvider.php
@@ -0,0 +1,72 @@
+<?php namespace Arcanedev\LaravelLang;
+
+use Arcanedev\Support\PackageServiceProvider as ServiceProvider;
+
+/**
+ * Class     LaravelLangServiceProvider
+ *
+ * @package  Arcanedev\LaravelLang
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class LaravelLangServiceProvider extends ServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Vendor name.
+     *
+     * @var string
+     */
+    protected $vendor  = 'arcanedev';
+
+    /**
+     * Package name.
+     *
+     * @var string
+     */
+    protected $package = 'laravel-lang';
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Get the base path of the package.
+     *
+     * @return string
+     */
+    public function getBasePath()
+    {
+        return dirname(__DIR__);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerConfig();
+
+        $this->app->register(TranslationServiceProvider::class);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            //
+        ];
+    }
+}
