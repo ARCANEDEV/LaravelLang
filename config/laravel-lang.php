e35b72cf716bedecd9ea7eaae325876c1e341773
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-01 20:24:28 +0000

    Updating config file

--- config/laravel-lang.php
+++ config/laravel-lang.php
@@ -2,4 +2,13 @@
 
 return [
     'vendor'    => base_path('vendor/caouecs/laravel4-lang'),
+
+    'check'     => [
+        'locales' => ['es', 'fr'],
+
+        'ignore'  => [
+            'validation.custom',
+            'validation.attributes',
+        ],
+    ],
 ];
