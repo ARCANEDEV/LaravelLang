931536ebf5fc16e2237d356026bd04a50c2ed9c9
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-01 15:37:12 +0000

    Adding the TranslationServiceProvider

---
+++ src/TranslationServiceProvider.php
@@ -0,0 +1,38 @@
+<?php namespace Arcanedev\LaravelLang;
+
+use Illuminate\Translation\TranslationServiceProvider as ServiceProvider;
+
+/**
+ * Class     TranslationServiceProvider
+ *
+ * @package  Arcanedev\LaravelLang\Providers
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class TranslationServiceProvider extends ServiceProvider
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Register the translation line loader.
+     *
+     * @return void
+     */
+    protected function registerLoader()
+    {
+        $this->app->singleton('translation.loader', function($app) {
+            /**
+             * @var  \Illuminate\Config\Repository      $config
+             * @var  \Illuminate\Filesystem\Filesystem  $files
+             */
+            $config   = $app['config'];
+            $files    = $app['files'];
+            $path     = $app['path.lang'];
+
+            return new FileLoader(
+                $files, $path, $config->get('laravel-lang.path')
+            );
+        });
+    }
+}
