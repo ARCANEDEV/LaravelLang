2e60ce0ace4db71dfc1733f6e4a45c897e8462e0
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-02 14:04:40 +0000

    Adding more fixtures

---
+++ tests/fixtures/lang/en/errors.php
@@ -0,0 +1,13 @@
+<?php
+//
+return [
+    '404' => [
+        'title' => 'Error 404 - Page not found'
+    ],
+    '500' => [
+        'title' => 'Error 500 - Internal Server Error'
+    ],
+    '503' => [
+        'title' => 'We will back soon.'
+    ],
+];
---
+++ tests/fixtures/lang/fr/main.php
@@ -0,0 +1,5 @@
+<?php
+//
+return [
+    'success' => 'Succès !',
+];
