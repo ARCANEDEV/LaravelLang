8f605b2a7d95bbc5ef0c52adcd7ced36e7836518
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-02 13:18:32 +0000

    Updating tests

--- tests/TransManagerTest.php
+++ tests/TransManagerTest.php
@@ -51,6 +51,43 @@ class TransManagerTest extends TestCase
             $this->assertInstanceOf($expected, $this->manager);
         }
 
+        $paths = $this->manager->getPaths();
         $this->assertCount(2, $this->manager->getPaths());
+        $this->assertArrayHasKey('app', $paths);
+        $this->assertArrayHasKey('vendor', $paths);
+    }
+
+    /** @test */
+    public function it_can_get_all_locales_keys()
+    {
+        $keys = $this->manager->keys();
+
+        foreach ($this->getLocales() as $locale) {
+            $this->assertArrayHasKey($locale, $keys);
+        }
+    }
+
+    /** @test */
+    public function it_can_count()
+    {
+        $this->assertEquals($this->getLocalesCount(), $this->manager->count());
+    }
+
+    /** @test */
+    public function it_can_get_locales_collection()
+    {
+        $expectations = [
+            'app'    => 1,
+            'vendor' => $this->getLocalesCount(false),
+        ];
+
+        foreach ($expectations as $group => $count) {
+            $locales = $this->manager->getCollection($group);
+
+            $this->assertInstanceOf(
+                \Arcanedev\LaravelLang\Entities\LocaleCollection::class, $locales
+            );
+            $this->assertCount($count, $locales);
+        }
     }
 }
